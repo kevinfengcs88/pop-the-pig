a6bd954797760457a4cde753c96f43ae43f19492
Kevin Feng <kevinfeng.cs88@gmail.com>
2022-04-04 08:56:35 -0400

    fixed burst size bug

--- src/components/Pig.js
+++ src/components/Pig.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
+let burstSize = Math.floor(Math.random() * (10 - 5) + 5);
 const Pig = () => {
     const [pigSize, setPigSize] = useState(1);
-    let burstSize = Math.floor(Math.random() * (20 - 5) * 5);
     const feedPig = () => {
         setPigSize(prevPigSize => prevPigSize + 1);
     }
     const restartGame = () => {
-        burstSize = Math.floor(Math.random() * (20 - 5) * 5);
+        burstSize = Math.floor(Math.random() * (10 - 5) + 5);
         setPigSize(1);
     }
   return (
