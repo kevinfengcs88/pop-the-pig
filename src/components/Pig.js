f8dc7ec8fa6682ba4a1a4953685eb7deed0d241e
Kevin Feng <kevinfeng.cs88@gmail.com>
2022-04-11 08:42:34 -0400

    adjusted pig's burst size to update with use state

--- src/components/Pig.js
+++ src/components/Pig.js
@@ -1,25 +1,30 @@
 import React, { useState } from 'react';
 import { Box, Button } from '@chakra-ui/react';
 
-let burstSize = Math.floor(Math.random() * (10 - 5) + 5);
 const Pig = () => {
     const [pigSize, setPigSize] = useState(1);
+    const [burstSize, setBurstSize] = useState(Math.floor(Math.random() * (10 - 5) + 5));
     const feedPig = () => {
         setPigSize(prevPigSize => prevPigSize + 1);
     }
     const restartGame = () => {
-        burstSize = Math.floor(Math.random() * (10 - 5) + 5);
+        setBurstSize(prevBurstSize => prevBurstSize - prevBurstSize + Math.floor(Math.random() * (10 - 5) + 5));
         setPigSize(1);
     }
   return (
     <>
-        <span>{burstSize}</span>
-        <span>{(pigSize >= burstSize) ? 'you lost' : pigSize}</span>
+        <span>Burst Size: {burstSize}</span>
+        <span>Pig Size: {(pigSize >= burstSize) ? 'you lost' : pigSize}</span>
         <Button 
           m='3'
           colorScheme={'red'} 
-        onClick={feedPig}>Feed</Button>
-        <Button onClick={restartGame}>Restart</Button>
+        onClick={feedPig}>
+          Feed
+        </Button>
+        <Button
+          onClick={restartGame}
+          colorScheme={'red'}
+        >Restart</Button>
     </>
   );
 };
